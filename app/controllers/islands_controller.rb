692f7778bca9d7ce7a2af25442a5a95575de8efc
Abraham <abdiaz2018@gmail.com>
2021-10-21 16:44:30 -0700

    added create method

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -1,2 +1,10 @@
 class IslandsController < ApplicationController
+    def create 
+        island = Island.create(island_params)
+        if island
+            render json: island 
+        else
+            render json: {error: 'This island could not be created'}
+        end
+    end
 end
