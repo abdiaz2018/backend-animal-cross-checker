61e7010c1e26909be7bcebb8bcac5174c935d994
Abraham <abdiaz2018@gmail.com>
2021-10-21 10:11:41 -0700

    adding create function

--- app/controllers/island_controller.rb
+++ app/controllers/island_controller.rb
@@ -1,4 +1,7 @@
 class IslandController < ApplicationController
+    def create 
+        island = Island.create(island_params)
+    end
     def index 
         islands = Island.all
         render json: islands
