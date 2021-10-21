eaee898d5fb6b37af6f9b2d6c0a2f9395b0df196
Abraham <abdiaz2018@gmail.com>
2021-10-21 16:49:00 -0700

    adding index method

--- app/controllers/villagers_controller.rb
+++ app/controllers/villagers_controller.rb
@@ -1,4 +1,10 @@
 class VillagersController < ApplicationController
+    def index
+        villagers = Villager.all
+        render json: villagers 
+    end
+
+    private 
 
     def villager_params
         params.require(:villager).permit(:name, :talked_to, :island_id)
