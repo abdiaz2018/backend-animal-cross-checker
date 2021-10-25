53a98285acc3bcf444842dc8c69426fc69470344
Abraham <abdiaz2018@gmail.com>
2021-10-25 16:28:42 -0700

    added nested route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :islands
+  resources :islands do
+    resources :villagers, only: [:index]
+  end
   resources :villagers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
